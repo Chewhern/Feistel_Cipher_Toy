ae7bc2bdc29c7f090d512cb64e5907ada5f80ac0
Chrono <chrono_legionnaire@protonmail.com>
2021-10-30 23:33:45 +0800

    Update FeistelCore.cs

--- Weak PRF/FeistelCore.cs
+++ Weak PRF/FeistelCore.cs
@@ -209,7 +209,7 @@ namespace FeistelToy
             return HMAC;
         }
 
-        public static Byte[] GenerateRoundKey(Byte[] Key) 
+        public static Byte[] GenerateRoundKey(Byte[] Key)
         {
             if (Key == null)
             {
@@ -222,88 +222,80 @@ namespace FeistelToy
                     throw new ArgumentException("Error: Key must exactly be 32 bytes in length");
                 }
             }
-            uint[] SubKeyUInt1 = new uint[4];
-            uint[] SubKeyUInt2 = new uint[4];
-            uint[] SubKeyUInt3 = new uint[4];
-            uint[] SubKeyUInt4 = new uint[4];
-            uint[] SubKeyUInt5 = new uint[4];
-            uint[] SubKeyUInt6 = new uint[4];
-            uint[] SubKeyUInt7 = new uint[4];
-            uint[] SubKeyUInt8 = new uint[4];
+            Byte[] TempKey1 = new Byte[4];
+            Byte[] TempKey2 = new Byte[4];
+            Byte[] TempKey3 = new Byte[4];
+            Byte[] TempKey4 = new Byte[4];
+            Byte[] TempKey5 = new Byte[4];
+            Byte[] TempKey6 = new Byte[4];
+            Byte[] TempKey7 = new Byte[4];
+            Byte[] TempKey8 = new Byte[4];
+            uint[] SubKeyUInt1 = new uint[2];
+            uint[] SubKeyUInt2 = new uint[2];
+            uint[] SubKeyUInt3 = new uint[2];
+            uint[] SubKeyUInt4 = new uint[2];
             Byte[] SubKey1 = new Byte[] { };
             Byte[] SubKey2 = new Byte[] { };
             Byte[] SubKey3 = new Byte[] { };
             Byte[] SubKey4 = new Byte[] { };
-            Byte[] SubKey5 = new Byte[] { };
-            Byte[] SubKey6 = new Byte[] { };
-            Byte[] SubKey7 = new Byte[] { };
-            Byte[] SubKey8 = new Byte[] { };
             Byte[] ConcatedSubKey = new Byte[] { };
             Byte[] MasterKey = new Byte[] { };
             int Loop = 0;
             int SubKeyUIntLoop = 0;
-            Array.Copy(Key, 0, SubKeyUInt1, 0, 4);
-            Array.Copy(Key, 4, SubKeyUInt2, 0, 4);
-            Array.Copy(Key, 8, SubKeyUInt3, 0, 4);
-            Array.Copy(Key, 12, SubKeyUInt4, 0, 4);
-            Array.Copy(Key, 16, SubKeyUInt5, 0, 4);
-            Array.Copy(Key, 20, SubKeyUInt6, 0, 4);
-            Array.Copy(Key, 24, SubKeyUInt7, 0, 4);
-            Array.Copy(Key, 28, SubKeyUInt8, 0, 4);
+            Array.Copy(Key, 0, TempKey1, 0, 4);
+            Array.Copy(Key, 4, TempKey2, 0, 4);
+            Array.Copy(Key, 8, TempKey3, 0, 4);
+            Array.Copy(Key, 12, TempKey4, 0, 4);
+            Array.Copy(Key, 16, TempKey5, 0, 4);
+            Array.Copy(Key, 20, TempKey6, 0, 4);
+            Array.Copy(Key, 24, TempKey7, 0, 4);
+            Array.Copy(Key, 28, TempKey8, 0, 4);
+            SubKeyUInt1[0] = BitConverter.ToUInt32(TempKey1);
+            SubKeyUInt1[1] = BitConverter.ToUInt32(TempKey2);
+            SubKeyUInt2[0] = BitConverter.ToUInt32(TempKey3);
+            SubKeyUInt2[1] = BitConverter.ToUInt32(TempKey4);
+            SubKeyUInt3[0] = BitConverter.ToUInt32(TempKey5);
+            SubKeyUInt3[1] = BitConverter.ToUInt32(TempKey6);
+            SubKeyUInt4[0] = BitConverter.ToUInt32(TempKey7);
+            SubKeyUInt4[1] = BitConverter.ToUInt32(TempKey8);
+            SodiumSecureMemory.SecureClearBytes(TempKey1);
+            SodiumSecureMemory.SecureClearBytes(TempKey2);
+            SodiumSecureMemory.SecureClearBytes(TempKey3);
+            SodiumSecureMemory.SecureClearBytes(TempKey4);
+            SodiumSecureMemory.SecureClearBytes(TempKey5);
+            SodiumSecureMemory.SecureClearBytes(TempKey6);
+            SodiumSecureMemory.SecureClearBytes(TempKey7);
+            SodiumSecureMemory.SecureClearBytes(TempKey8);
             while (Loop < 16)
             {
                 SubKeyUInt1[SubKeyUIntLoop] ^= SubKeyUInt2[SubKeyUIntLoop];
                 SubKeyUInt2[SubKeyUIntLoop] ^= SubKeyUInt3[SubKeyUIntLoop];
                 SubKeyUInt3[SubKeyUIntLoop] ^= SubKeyUInt4[SubKeyUIntLoop];
-                SubKeyUInt4[SubKeyUIntLoop] ^= SubKeyUInt5[SubKeyUIntLoop];
-                SubKeyUInt5[SubKeyUIntLoop] ^= SubKeyUInt6[SubKeyUIntLoop];
-                SubKeyUInt6[SubKeyUIntLoop] ^= SubKeyUInt7[SubKeyUIntLoop];
-                SubKeyUInt7[SubKeyUIntLoop] ^= SubKeyUInt8[SubKeyUIntLoop];
-                SubKeyUInt8[SubKeyUIntLoop] ^= SubKeyUInt1[SubKeyUIntLoop];
-                SubKeyUInt1[SubKeyUIntLoop] = SubKeyUInt1[SubKeyUIntLoop] >> 4;
-                SubKeyUInt2[SubKeyUIntLoop] = SubKeyUInt2[SubKeyUIntLoop] >> 4;
-                SubKeyUInt3[SubKeyUIntLoop] = SubKeyUInt3[SubKeyUIntLoop] >> 4;
-                SubKeyUInt4[SubKeyUIntLoop] = SubKeyUInt4[SubKeyUIntLoop] >> 4;
-                SubKeyUInt5[SubKeyUIntLoop] = SubKeyUInt5[SubKeyUIntLoop] >> 4;
-                SubKeyUInt6[SubKeyUIntLoop] = SubKeyUInt6[SubKeyUIntLoop] >> 4;
-                SubKeyUInt7[SubKeyUIntLoop] = SubKeyUInt7[SubKeyUIntLoop] >> 4;
-                SubKeyUInt8[SubKeyUIntLoop] = SubKeyUInt8[SubKeyUIntLoop] >> 4;
+                SubKeyUInt4[SubKeyUIntLoop] ^= SubKeyUInt1[SubKeyUIntLoop];
+                SubKeyUInt1[SubKeyUIntLoop] = SubKeyUInt1[SubKeyUIntLoop] >> 2;
+                SubKeyUInt2[SubKeyUIntLoop] = SubKeyUInt2[SubKeyUIntLoop] >> 2;
+                SubKeyUInt3[SubKeyUIntLoop] = SubKeyUInt3[SubKeyUIntLoop] >> 2;
+                SubKeyUInt4[SubKeyUIntLoop] = SubKeyUInt4[SubKeyUIntLoop] >> 2;
                 SubKeyUInt1[SubKeyUIntLoop] ^= SubKeyUInt2[SubKeyUIntLoop];
                 SubKeyUInt2[SubKeyUIntLoop] ^= SubKeyUInt3[SubKeyUIntLoop];
                 SubKeyUInt3[SubKeyUIntLoop] ^= SubKeyUInt4[SubKeyUIntLoop];
-                SubKeyUInt4[SubKeyUIntLoop] ^= SubKeyUInt5[SubKeyUIntLoop];
-                SubKeyUInt5[SubKeyUIntLoop] ^= SubKeyUInt6[SubKeyUIntLoop];
-                SubKeyUInt6[SubKeyUIntLoop] ^= SubKeyUInt7[SubKeyUIntLoop];
-                SubKeyUInt7[SubKeyUIntLoop] ^= SubKeyUInt8[SubKeyUIntLoop];
-                SubKeyUInt8[SubKeyUIntLoop] ^= SubKeyUInt1[SubKeyUIntLoop];
+                SubKeyUInt4[SubKeyUIntLoop] ^= SubKeyUInt1[SubKeyUIntLoop];
                 SubKeyUInt1[SubKeyUIntLoop] = SubKeyUInt1[SubKeyUIntLoop] << 4;
                 SubKeyUInt2[SubKeyUIntLoop] = SubKeyUInt2[SubKeyUIntLoop] << 4;
                 SubKeyUInt3[SubKeyUIntLoop] = SubKeyUInt3[SubKeyUIntLoop] << 4;
                 SubKeyUInt4[SubKeyUIntLoop] = SubKeyUInt4[SubKeyUIntLoop] << 4;
-                SubKeyUInt5[SubKeyUIntLoop] = SubKeyUInt5[SubKeyUIntLoop] << 4;
-                SubKeyUInt6[SubKeyUIntLoop] = SubKeyUInt6[SubKeyUIntLoop] << 4;
-                SubKeyUInt7[SubKeyUIntLoop] = SubKeyUInt7[SubKeyUIntLoop] << 4;
-                SubKeyUInt8[SubKeyUIntLoop] = SubKeyUInt8[SubKeyUIntLoop] << 4;
                 SubKeyUInt1[SubKeyUIntLoop] ^= SubKeyUInt2[SubKeyUIntLoop];
                 SubKeyUInt2[SubKeyUIntLoop] ^= SubKeyUInt3[SubKeyUIntLoop];
                 SubKeyUInt3[SubKeyUIntLoop] ^= SubKeyUInt4[SubKeyUIntLoop];
-                SubKeyUInt4[SubKeyUIntLoop] ^= SubKeyUInt5[SubKeyUIntLoop];
-                SubKeyUInt5[SubKeyUIntLoop] ^= SubKeyUInt6[SubKeyUIntLoop];
-                SubKeyUInt6[SubKeyUIntLoop] ^= SubKeyUInt7[SubKeyUIntLoop];
-                SubKeyUInt7[SubKeyUIntLoop] ^= SubKeyUInt8[SubKeyUIntLoop];
-                SubKeyUInt8[SubKeyUIntLoop] ^= SubKeyUInt1[SubKeyUIntLoop];
+                SubKeyUInt4[SubKeyUIntLoop] ^= SubKeyUInt1[SubKeyUIntLoop];
                 SubKeyUInt1[SubKeyUIntLoop] = SubKeyUInt2[SubKeyUIntLoop];
                 SubKeyUInt2[SubKeyUIntLoop] = SubKeyUInt3[SubKeyUIntLoop];
                 SubKeyUInt3[SubKeyUIntLoop] = SubKeyUInt4[SubKeyUIntLoop];
-                SubKeyUInt4[SubKeyUIntLoop] = SubKeyUInt5[SubKeyUIntLoop];
-                SubKeyUInt5[SubKeyUIntLoop] = SubKeyUInt6[SubKeyUIntLoop];
-                SubKeyUInt6[SubKeyUIntLoop] = SubKeyUInt7[SubKeyUIntLoop];
-                SubKeyUInt7[SubKeyUIntLoop] = SubKeyUInt8[SubKeyUIntLoop];
-                SubKeyUInt8[SubKeyUIntLoop] = SubKeyUInt1[SubKeyUIntLoop] >> 4;
-                SubKeyUInt8[SubKeyUIntLoop] ^= SubKeyUInt2[SubKeyUIntLoop];
-                SubKeyUInt8[SubKeyUIntLoop] <<= 4;
-                SubKeyUInt8[SubKeyUIntLoop] ^= SubKeyUInt3[SubKeyUIntLoop];
-                if (SubKeyUIntLoop == 3)
+                SubKeyUInt4[SubKeyUIntLoop] ^= SubKeyUInt1[SubKeyUIntLoop] >> 4;
+                SubKeyUInt4[SubKeyUIntLoop] ^= SubKeyUInt2[SubKeyUIntLoop];
+                SubKeyUInt4[SubKeyUIntLoop] <<= 2;
+                SubKeyUInt4[SubKeyUIntLoop] ^= SubKeyUInt3[SubKeyUIntLoop];
+                if (SubKeyUIntLoop == 1)
                 {
                     SubKeyUIntLoop = 0;
                 }
@@ -315,46 +307,26 @@ namespace FeistelToy
             while (Loop < 16)
             {
                 SubKeyUInt1[SubKeyUIntLoop] ^= SubKeyUInt2[SubKeyUIntLoop];
-                SubKeyUInt1[SubKeyUIntLoop] = SubKeyUInt1[SubKeyUIntLoop] >> 4;
+                SubKeyUInt1[SubKeyUIntLoop] = SubKeyUInt1[SubKeyUIntLoop] >> 2;
                 SubKeyUInt1[SubKeyUIntLoop] ^= SubKeyUInt3[SubKeyUIntLoop];
-                SubKeyUInt1[SubKeyUIntLoop] = SubKeyUInt1[SubKeyUIntLoop] << 6;
+                SubKeyUInt1[SubKeyUIntLoop] = SubKeyUInt1[SubKeyUIntLoop] << 4;
                 SubKeyUInt1[SubKeyUIntLoop] ^= SubKeyUInt4[SubKeyUIntLoop];
                 SubKeyUInt2[SubKeyUIntLoop] ^= SubKeyUInt3[SubKeyUIntLoop];
-                SubKeyUInt2[SubKeyUIntLoop] = SubKeyUInt2[SubKeyUIntLoop] >> 4;
+                SubKeyUInt2[SubKeyUIntLoop] = SubKeyUInt2[SubKeyUIntLoop] >> 2;
                 SubKeyUInt2[SubKeyUIntLoop] ^= SubKeyUInt4[SubKeyUIntLoop];
-                SubKeyUInt2[SubKeyUIntLoop] = SubKeyUInt2[SubKeyUIntLoop] << 6;
-                SubKeyUInt2[SubKeyUIntLoop] ^= SubKeyUInt5[SubKeyUIntLoop];
+                SubKeyUInt2[SubKeyUIntLoop] = SubKeyUInt2[SubKeyUIntLoop] << 4;
+                SubKeyUInt2[SubKeyUIntLoop] ^= SubKeyUInt1[SubKeyUIntLoop];
                 SubKeyUInt3[SubKeyUIntLoop] ^= SubKeyUInt4[SubKeyUIntLoop];
-                SubKeyUInt3[SubKeyUIntLoop] = SubKeyUInt3[SubKeyUIntLoop] >> 4;
-                SubKeyUInt3[SubKeyUIntLoop] ^= SubKeyUInt5[SubKeyUIntLoop];
-                SubKeyUInt3[SubKeyUIntLoop] = SubKeyUInt3[SubKeyUIntLoop] << 6;
-                SubKeyUInt3[SubKeyUIntLoop] ^= SubKeyUInt6[SubKeyUIntLoop];
-                SubKeyUInt4[SubKeyUIntLoop] ^= SubKeyUInt5[SubKeyUIntLoop];
-                SubKeyUInt4[SubKeyUIntLoop] = SubKeyUInt4[SubKeyUIntLoop] >> 4;
-                SubKeyUInt4[SubKeyUIntLoop] ^= SubKeyUInt6[SubKeyUIntLoop];
-                SubKeyUInt4[SubKeyUIntLoop] = SubKeyUInt4[SubKeyUIntLoop] << 6;
-                SubKeyUInt4[SubKeyUIntLoop] ^= SubKeyUInt7[SubKeyUIntLoop];
-                SubKeyUInt5[SubKeyUIntLoop] ^= SubKeyUInt6[SubKeyUIntLoop];
-                SubKeyUInt5[SubKeyUIntLoop] = SubKeyUInt5[SubKeyUIntLoop] >> 4;
-                SubKeyUInt5[SubKeyUIntLoop] ^= SubKeyUInt7[SubKeyUIntLoop];
-                SubKeyUInt5[SubKeyUIntLoop] = SubKeyUInt5[SubKeyUIntLoop] << 6;
-                SubKeyUInt5[SubKeyUIntLoop] ^= SubKeyUInt8[SubKeyUIntLoop];
-                SubKeyUInt6[SubKeyUIntLoop] ^= SubKeyUInt7[SubKeyUIntLoop];
-                SubKeyUInt6[SubKeyUIntLoop] = SubKeyUInt6[SubKeyUIntLoop] >> 4;
-                SubKeyUInt6[SubKeyUIntLoop] ^= SubKeyUInt8[SubKeyUIntLoop];
-                SubKeyUInt6[SubKeyUIntLoop] = SubKeyUInt6[SubKeyUIntLoop] << 6;
-                SubKeyUInt6[SubKeyUIntLoop] ^= SubKeyUInt1[SubKeyUIntLoop];
-                SubKeyUInt7[SubKeyUIntLoop] ^= SubKeyUInt8[SubKeyUIntLoop];
-                SubKeyUInt7[SubKeyUIntLoop] = SubKeyUInt7[SubKeyUIntLoop] >> 4;
-                SubKeyUInt7[SubKeyUIntLoop] ^= SubKeyUInt1[SubKeyUIntLoop];
-                SubKeyUInt7[SubKeyUIntLoop] = SubKeyUInt7[SubKeyUIntLoop] << 6;
-                SubKeyUInt7[SubKeyUIntLoop] ^= SubKeyUInt2[SubKeyUIntLoop];
-                SubKeyUInt8[SubKeyUIntLoop] ^= SubKeyUInt1[SubKeyUIntLoop];
-                SubKeyUInt8[SubKeyUIntLoop] = SubKeyUInt8[SubKeyUIntLoop] >> 4;
-                SubKeyUInt8[SubKeyUIntLoop] ^= SubKeyUInt2[SubKeyUIntLoop];
-                SubKeyUInt8[SubKeyUIntLoop] = SubKeyUInt8[SubKeyUIntLoop] << 6;
-                SubKeyUInt8[SubKeyUIntLoop] ^= SubKeyUInt3[SubKeyUIntLoop];
-                if (SubKeyUIntLoop == 3)
+                SubKeyUInt3[SubKeyUIntLoop] = SubKeyUInt3[SubKeyUIntLoop] >> 2;
+                SubKeyUInt3[SubKeyUIntLoop] ^= SubKeyUInt1[SubKeyUIntLoop];
+                SubKeyUInt3[SubKeyUIntLoop] = SubKeyUInt3[SubKeyUIntLoop] << 4;
+                SubKeyUInt3[SubKeyUIntLoop] ^= SubKeyUInt2[SubKeyUIntLoop];
+                SubKeyUInt4[SubKeyUIntLoop] ^= SubKeyUInt1[SubKeyUIntLoop];
+                SubKeyUInt4[SubKeyUIntLoop] = SubKeyUInt4[SubKeyUIntLoop] >> 2;
+                SubKeyUInt4[SubKeyUIntLoop] ^= SubKeyUInt2[SubKeyUIntLoop];
+                SubKeyUInt4[SubKeyUIntLoop] = SubKeyUInt4[SubKeyUIntLoop] << 4;
+                SubKeyUInt4[SubKeyUIntLoop] ^= SubKeyUInt3[SubKeyUIntLoop];
+                if (SubKeyUIntLoop == 1)
                 {
                     SubKeyUIntLoop = 0;
                 }
@@ -362,20 +334,15 @@ namespace FeistelToy
                 Loop += 1;
             }
             Loop = 0;
-            while (Loop < 4)
+            while (Loop < 2)
             {
                 SubKey1 = SubKey1.Concat(ConvertUIntToByteArray(SubKeyUInt1[Loop])).ToArray();
                 SubKey2 = SubKey2.Concat(ConvertUIntToByteArray(SubKeyUInt2[Loop])).ToArray();
                 SubKey3 = SubKey3.Concat(ConvertUIntToByteArray(SubKeyUInt3[Loop])).ToArray();
                 SubKey4 = SubKey4.Concat(ConvertUIntToByteArray(SubKeyUInt4[Loop])).ToArray();
-                SubKey5 = SubKey5.Concat(ConvertUIntToByteArray(SubKeyUInt5[Loop])).ToArray();
-                SubKey6 = SubKey6.Concat(ConvertUIntToByteArray(SubKeyUInt6[Loop])).ToArray();
-                SubKey7 = SubKey7.Concat(ConvertUIntToByteArray(SubKeyUInt7[Loop])).ToArray();
-                SubKey8 = SubKey8.Concat(ConvertUIntToByteArray(SubKeyUInt8[Loop])).ToArray();
                 Loop += 1;
             }
-            ConcatedSubKey = SubKey1.Concat(SubKey2).Concat(SubKey3).Concat(SubKey4).
-                Concat(SubKey5).Concat(SubKey6).Concat(SubKey7).Concat(SubKey8).ToArray();
+            ConcatedSubKey = SubKey1.Concat(SubKey2).Concat(SubKey3).Concat(SubKey4).ToArray();
             MasterKey = SodiumKDF.KDFFunction(32, 1, "KDFForFC", ConcatedSubKey, true);
 
             GCHandle MyGeneralGCHandle = GCHandle.Alloc(SubKeyUInt1, GCHandleType.Pinned);
@@ -390,27 +357,11 @@ namespace FeistelToy
             MyGeneralGCHandle = GCHandle.Alloc(SubKeyUInt4, GCHandleType.Pinned);
             SodiumSecureMemory.MemZero(MyGeneralGCHandle.AddrOfPinnedObject(), SubKeyUInt4.Length * 4);
             MyGeneralGCHandle.Free();
-            MyGeneralGCHandle = GCHandle.Alloc(SubKeyUInt5, GCHandleType.Pinned);
-            SodiumSecureMemory.MemZero(MyGeneralGCHandle.AddrOfPinnedObject(), SubKeyUInt5.Length * 4);
-            MyGeneralGCHandle.Free();
-            MyGeneralGCHandle = GCHandle.Alloc(SubKeyUInt6, GCHandleType.Pinned);
-            SodiumSecureMemory.MemZero(MyGeneralGCHandle.AddrOfPinnedObject(), SubKeyUInt6.Length * 4);
-            MyGeneralGCHandle.Free();
-            MyGeneralGCHandle = GCHandle.Alloc(SubKeyUInt7, GCHandleType.Pinned);
-            SodiumSecureMemory.MemZero(MyGeneralGCHandle.AddrOfPinnedObject(), SubKeyUInt7.Length * 4);
-            MyGeneralGCHandle.Free();
-            MyGeneralGCHandle = GCHandle.Alloc(SubKeyUInt8, GCHandleType.Pinned);
-            SodiumSecureMemory.MemZero(MyGeneralGCHandle.AddrOfPinnedObject(), SubKeyUInt8.Length * 4);
-            MyGeneralGCHandle.Free();
 
             SodiumSecureMemory.SecureClearBytes(SubKey1);
             SodiumSecureMemory.SecureClearBytes(SubKey2);
             SodiumSecureMemory.SecureClearBytes(SubKey3);
             SodiumSecureMemory.SecureClearBytes(SubKey4);
-            SodiumSecureMemory.SecureClearBytes(SubKey5);
-            SodiumSecureMemory.SecureClearBytes(SubKey6);
-            SodiumSecureMemory.SecureClearBytes(SubKey7);
-            SodiumSecureMemory.SecureClearBytes(SubKey8);
             return MasterKey;
         }
 
